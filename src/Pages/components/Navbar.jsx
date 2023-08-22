64fa20d0db6c56ad7515e0451e9b79f2dd4329ae
m9heb <m.moheb1382@gmail.com>
2023-08-22 09:21:39 +0430

    search functionality added

--- src/Pages/components/Navbar.jsx
+++ src/Pages/components/Navbar.jsx
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
 import { signOut } from "firebase/auth";
 import { auth } from "../../firebase";
+import { AuthContext } from "../../context/AuthContext";
 const Navbar = () => {
+  const { currentUser } = useContext(AuthContext);
   return (
     <div className="navbar">
       <span className="logo">MChat</span>
       <div className="user">
-        <img src="" alt="" />
-        <span>John</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>Log out</button>
       </div>
     </div>
--- src/Pages/components/Search.jsx
+++ src/Pages/components/Search.jsx
@@ -1,18 +1,92 @@
-import React from "react";
-
+import React, { useContext, useState } from "react";
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  getDoc,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+} from "firebase/firestore";
+import { db } from "../../firebase";
+import { AuthContext } from "../../context/AuthContext";
 const Search = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+  const { currentUser } = useContext(AuthContext);
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+  const handleSelect = async () => {
+    const combinedId =
+      currentUser.uid > user.uid
+        ? currentUser.uid + user.uid
+        : user.uid + currentUser.uid;
+
+    const res = await getDoc(doc(db, "chats", combinedId));
+
+    if (!res.exists()) {
+      await setDoc(doc(db, "chats", combinedId), { messages: [] });
+      // create user chats
+      await updateDoc(doc(db, "userChats", currentUser.uid), {
+        [combinedId + ".userInfo"]: {
+          uid: user.uid,
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+        },
+        [combinedId + ".date"]: serverTimestamp(),
+      });
+      await updateDoc(doc(db, "userChats", user.uid), {
+        [combinedId + ".userInfo"]: {
+          uid: currentUser.uid,
+          displayName: currentUser.displayName,
+          photoURL: currentUser.photoURL,
+        },
+        [combinedId + ".date"]: serverTimestamp(),
+      });
+    }
+    setUser(null);
+    setUsername("");
+  };
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
-        <input type="search" placeholder="find a user" />
+        <input
+          type="text"
+          placeholder="find a user"
+          onKeyDown={handleKey}
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
-      <div className="userChat">
-        <img src="" alt="" />
-        <div className="userChatInfo">
-          <span>Jane</span>
-          <p>Hello</p>
+      {err && <span>User not found!</span>}
+      {user && (
+        <div className="userChat" onClick={handleSelect}>
+          <img src={user.photoURL} alt="" />
+          <div className="userChatInfo">
+            <span>{user.displayName}</span>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
